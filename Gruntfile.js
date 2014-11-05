a353b62fd8f0fb48a7d508e432adceb9f7dd73cd
Alex Chao <alexchao1990@gmail.com>
2014-11-05 22:45:50 +0800

    configure package.json & grunt & gitignore

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+/**
+ * overlay
+ * https://github.com/Alex1990/overlay
+ *
+ * Copyright (c) 2014 Alex Chao
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt){
+
+  // Load all grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'src/*.js'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+    uglify: {
+      build: {
+        files: [
+          {
+            expand: true,
+            cwd: 'src',    
+            src: '**/*.js',
+            dest: 'dist'
+          }
+        ]
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['jshint', 'uglify']);
+
+};
